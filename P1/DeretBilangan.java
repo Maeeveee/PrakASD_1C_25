3ee35265f7108029c32673ac1f9b7645c696f3c7
Maeeveee <142636445+Maeeveee@users.noreply.github.com>
2024-02-15 08:31:11 +0700

    Perbaikan_P1_Percobaan2_1C_25

--- P1/DeretBilangan.java
+++ P1/DeretBilangan.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class DeretBilangan {
     public static void main(String[] args) {
         Scanner sc25 = new Scanner(System.in);
-        int NIM=0;
-        System.out.println("Masukkan 2 digit terakhir NIM anda : ");
-        NIM = sc25.nextInt();
-        for (int i = 1; i <= NIM; i++) {            
+        long NIM=0;
+        System.out.print("Masukkan NIM anda : ");
+        NIM = sc25.nextLong();
+        int HasilNIM =(int) (NIM % 100);
+        for (int i = 1; i <= HasilNIM; i++) {            
             if (i%2 ==0) {
                 System.out.print(i);
                 System.out.print(" * ");

dfa7be61bbf9122ba7dad017625c44641beb36c4
Maeeveee <142636445+Maeeveee@users.noreply.github.com>
2024-02-29 09:42:37 +0700

    Pertanyaan 5 Percobaan 3 P2_1C_25

--- P2/PenjualanBuku/BukuMain25.java
+++ P2/PenjualanBuku/BukuMain25.java
@@ -14,8 +14,11 @@ public class BukuMain25 {
         bk1.gantiharga(60000);
         bk1.tampilInformasi();
 
-        Buku25 bk2 = new Buku25("Self Reward","maheera ayesha",160,29,59000);
-        bk2.terjual(11);
-        bk2.tampilInformasi();
-    }
+        // Buku25 bk2 = new Buku25("Self Reward","maheera ayesha",160,29,59000);
+        // bk2.terjual(11);
+        // bk2.tampilInformasi();
+        Buku25 RizalAbrar = new Buku25("Rahasia lolos snbt","Rizal Abrar",200,30,100_000);
+        RizalAbrar.tampilInformasi();
 }
+    }
+        

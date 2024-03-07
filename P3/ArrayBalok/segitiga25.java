490e803469196792e1426b8be0b735b440642c45
Maeeveee <142636445+Maeeveee@users.noreply.github.com>
2024-03-07 10:03:02 +0700

    P3_Pertanyaan 2_Percobaan3_1C_25

---
+++ P3/ArrayBalok/segitiga25.java
@@ -0,0 +1,11 @@
+package P3.ArrayBalok;
+
+public class segitiga25 {
+    public int alas;
+    public int tinggi;
+
+    public segitiga25(int a, int t){
+       alas = a;
+       tinggi = t; 
+    }
+}

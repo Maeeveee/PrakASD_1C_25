8b68fb899af9e899b3b4b47505fbd1da3e112069
Maeeveee <142636445+Maeeveee@users.noreply.github.com>
2024-03-07 11:26:28 +0700

    P3_Pertanyaan 4_percobaan 3_1C_25

---
+++ P3/ArrayBalok/Segitiga25Main.java
@@ -0,0 +1,14 @@
+package P3.ArrayBalok;
+
+public class Segitiga25Main {
+    public static void main(String[] args) {
+    segitiga25[] sgArray25 = new segitiga25[4];
+    
+    sgArray25[0] = new segitiga25(10 , 4);
+    sgArray25[1] = new segitiga25(20 , 10);
+    sgArray25[2] = new segitiga25(15 ,6);
+    sgArray25[3] = new segitiga25(25 , 10);
+
+    
+    }
+}
--- P3/ArrayBalok/segitiga25.java
+++ P3/ArrayBalok/segitiga25.java
@@ -8,8 +8,8 @@ public class segitiga25 {
        alas = a;
        tinggi = t; 
     }
-    public double hitungLuas(int a, int t){
-        return (a*t)/2;
+    public double hitungLuas(){
+        return (alas*tinggi)/2;
     }
     public double hitungKeliling(){
         double sisiMiring = Math.sqrt(Math.pow(alas, 2) + Math.pow(tinggi, 2));

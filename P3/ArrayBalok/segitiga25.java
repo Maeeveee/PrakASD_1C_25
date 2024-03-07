14e6798936c9245ec948f16bc2aa16e43c5ca902
Maeeveee <142636445+Maeeveee@users.noreply.github.com>
2024-03-07 11:12:19 +0700

    P3_pertanyaan 3_percobaan 3_1C_25

--- P3/ArrayBalok/segitiga25.java
+++ P3/ArrayBalok/segitiga25.java
@@ -8,4 +8,13 @@ public class segitiga25 {
        alas = a;
        tinggi = t; 
     }
+    public double hitungLuas(int a, int t){
+        return (a*t)/2;
+    }
+    public double hitungKeliling(){
+        double sisiMiring = Math.sqrt(Math.pow(alas, 2) + Math.pow(tinggi, 2));
+        double keliling = alas + tinggi + sisiMiring;
+        return keliling;
+    }
+    
 }

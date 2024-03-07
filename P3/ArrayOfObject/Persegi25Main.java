fa8983813282282dab9907fc4cafdb0d181ba431
Maeeveee <142636445+Maeeveee@users.noreply.github.com>
2024-03-07 09:18:27 +0700

    P3_Perbaikan pertanyaan 3 percobaan 2_1C_25

--- P3/ArrayOfObject/Persegi25Main.java
+++ P3/ArrayOfObject/Persegi25Main.java
@@ -3,6 +3,7 @@ package P3.ArrayOfObject;
 public class Persegi25Main {
     public static void main(String[] args) {
         Persegi25[] pgArray = new Persegi25[100];
+        pgArray[5] = new Persegi25();
         pgArray[5].sisi = 20;
         System.out.println(pgArray[5].sisi);
     }

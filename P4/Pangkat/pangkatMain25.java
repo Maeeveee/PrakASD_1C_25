995f0d9dc1f7b3a8142669f3df658ac3c205a2b4
Maeeveee <142636445+Maeeveee@users.noreply.github.com>
2024-03-21 04:29:34 +0700

    P4_Percobaan2_pertanyaan4_1C_25

--- P4/Pangkat/pangkatMain25.java
+++ P4/Pangkat/pangkatMain25.java
@@ -18,14 +18,31 @@ public class pangkatMain25 {
 
             png[i] = new pangkat25(nilai, pangkat);
         }
-        System.out.println("Hasil Pangkat - Brute Force");
-        for (int j = 0; j < elemen; j++) {
-            System.out.println("Hasil Dari "+png[j].nilai+" pangkat "+png[j].pangkat+" adalah "+png[j].pangkatBF(png[j].nilai, png[j].pangkat));
-        }
-        System.out.println("Hasil Pangkat -Divide And Conquer");
-        for (int i = 0; i < png.length; i++) {
-            System.out.println("Hasil Dari "+png[i].nilai+" pangkat "+png[i].pangkat+" adalah "+png[i].pangkatDC(png[i].nilai, png[i].pangkat));
+        System.out.println("Pilih Menu Pemangkatan");
+        System.out.println("1. Brute Force");
+        System.out.println("2. Divide And Conquer");
+        System.out.print("Pilih");
+        int pilih = sc25.nextInt();
+        switch (pilih) {
+            case 1:
+            System.out.println("Hasil Pangkat - Brute Force");
+            for (int j = 0; j < elemen; j++) {
+                System.out.println("Hasil Dari "+png[j].nilai+" pangkat "+png[j].pangkat+" adalah "+png[j].pangkatBF(png[j].nilai, png[j].pangkat));
+            }
+                break;
             
+            case 2:
+            System.out.println("Hasil Pangkat -Divide And Conquer");
+            for (int i = 0; i < png.length; i++) {
+                System.out.println("Hasil Dari "+png[i].nilai+" pangkat "+png[i].pangkat+" adalah "+png[i].pangkatDC(png[i].nilai, png[i].pangkat));
+                
+            }
+                break;
+            
+            default:
+            System.out.println("Pilihan tidak tersedia");
+                break;
         }
+       
     }
 }

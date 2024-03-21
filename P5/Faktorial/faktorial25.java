daef40a64d0132ecd99e25c3dcba5291ff05fcbd
Maeeveee <142636445+Maeeveee@users.noreply.github.com>
2024-03-21 09:26:05 +0700

    P5_Percobaan 2_Perbaikan Package

---
+++ P5/Faktorial/faktorial25.java
@@ -0,0 +1,30 @@
+package P5.Faktorial;
+
+public class faktorial25 {
+    public int nilai;
+    
+public faktorial25(int n){
+    nilai = n;
+}
+
+public int FaktorialBF(int n){
+    int fakto = 1;
+    int i = 1;
+    while (i <= n) {
+        fakto *= i;
+        i++;
+    }
+    return fakto;
+}
+
+public int FaktorialDC(int n){
+    if (n==1) {
+        return 1;
+    } 
+    else {
+        int fakto = n * FaktorialDC(n-1);
+        return fakto;
+    }
+}
+
+}

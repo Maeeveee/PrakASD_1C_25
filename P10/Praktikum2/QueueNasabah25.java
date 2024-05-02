fc25b1302c62ab624b10a634673503177e51462a
Maeeveee <142636445+Maeeveee@users.noreply.github.com>
2024-05-02 10:18:55 +0700

    P10_Percobaan2_1C_25

---
+++ P10/Praktikum2/QueueNasabah25.java
@@ -0,0 +1,97 @@
+package P10.Praktikum2;
+
+public class QueueNasabah25 {
+    Nasabah25 data[];
+    int front;
+    int rear;
+    int size;
+    int max;
+
+    public QueueNasabah25(int n){
+        max =  n;
+        data = new Nasabah25[max];
+        size = 0;
+        front = rear = -1;
+    }
+    public boolean isEmpty(){
+        if (size == 0 ) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+    public boolean isFull(){
+        if (size == max) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public void peek(){
+        if(!isEmpty()){
+            System.out.println("elemen Terdepan: "+data[front].norek+" "+data[front].nama+" "+data[front].alamat+" "+data[front].umur+" "+data[front].saldo);
+        } else {
+            System.out.println("Queue masih kosong");
+        }
+    }
+    public void print(){
+        if (isEmpty()) {
+            System.out.println("Queue masih kosong");
+        }else {
+            int i = front;
+            while (i != rear) {
+                System.out.print(data[i].norek+" "+data[i].nama+" "+data[i].alamat+" "+data[i].umur+" "+data[i].saldo);
+                i = (i + 1) % max;
+            }
+            System.out.println(data[i].norek+" "+data[i].nama+" "+data[i].alamat+" "+data[i].umur+" "+data[i].saldo);
+            System.out.println("jumlah elemen = "+size);
+        }
+    }
+    public void clear(){
+        if (isEmpty()) {
+            front = rear = -1;
+            size = 0;
+            System.out.println("queue berhasil dikosongkan");
+        }else {
+            System.out.println("Queue masih kosong");
+        }
+    }
+    public void enqueue(Nasabah25 dt){
+        if (isFull()) {
+            System.out.println("Queue sudah penuh");
+            System.exit(0);
+        }else{
+            if (isEmpty()) {
+                front = rear =0;
+            }else{
+                if (rear == max -1) {
+                    rear = 0;
+                }else{
+                    rear++;
+                }
+            }
+            data[rear] = dt;
+            size ++;
+        }
+    }
+    public Nasabah25 dequeue(){
+        Nasabah25 dt = new Nasabah25();
+        if (isEmpty()) {
+            System.out.println("Queue masih kosong");
+            System.exit(0);
+        }else {
+            dt = data[front];
+            size--;
+        if (isEmpty()) {
+            front = rear = -1;
+        }else{
+            if (front == max -1) {
+                front = 0;
+            }else{
+                front++;
+            }
+        }
+        }
+        return dt;
+    }
+}
---
+++ P10/Praktikum2/QueueNasabahMain25.java
@@ -0,0 +1,58 @@
+package P10.Praktikum2;
+import java.util.Scanner;
+public class QueueNasabahMain25 {
+    public static void menu(){
+        System.out.println("Pilihan menu : ");
+        System.out.println("1. Antrian baru");
+        System.out.println("2. Antrian keluar");
+        System.out.println("3. Cek antrian terdepan");
+        System.out.println("4. Cek semua antrian");
+    }
+    public static void main(String[] args) {
+        Scanner sc25 =  new Scanner(System.in);
+        System.out.print("Masukkan kapasitas Queue : ");
+        int jumlah  = sc25.nextInt();
+        QueueNasabah25 antri = new QueueNasabah25(jumlah);
+
+        int pilih;
+
+        do {
+            menu();
+            pilih = sc25.nextInt();
+            sc25.nextLine();
+            switch (pilih) {
+                case 1:
+                    System.out.print("No Rekening : ");
+                    String norek = sc25.nextLine();
+                    System.out.print("No Rekening : ");
+                    String nama = sc25.nextLine();
+                    System.out.print("No Rekening : ");
+                    String alamat = sc25.nextLine();
+                    System.out.print("No Rekening : ");
+                    int umur = sc25.nextInt();
+                    sc25.nextLine();
+                    System.out.print("No Rekening : ");
+                    double saldo = sc25.nextDouble();
+
+                    Nasabah25 nb = new Nasabah25(norek, nama, alamat, umur, saldo);
+                    sc25.nextLine();
+                    antri.enqueue(nb);
+                    break;
+                case 2: 
+                    Nasabah25 data = antri.dequeue();
+                    if (!"".equals(data.norek)&&!"".equals(data.nama)&&!"".equals(data.alamat)&&data.umur != 0 && data.saldo !=0) {
+                        System.out.println("Antrian yang keluar : "+data.norek+" "+data.nama+" "+data.alamat+" "+data.umur+" "+data.alamat);
+                    }
+                    break;
+                case 3:
+                antri.peek();
+                    break;
+                case 4:
+                antri.print();
+                    break;
+                default:
+                    break;
+            }
+        } while (pilih == 1||pilih == 2||pilih == 3||pilih == 4);
+    }
+}

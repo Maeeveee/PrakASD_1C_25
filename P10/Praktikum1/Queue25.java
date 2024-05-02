776273b63b85b09005e47c7e551a7c5c6fb3ff55
Maeeveee <142636445+Maeeveee@users.noreply.github.com>
2024-05-02 09:42:51 +0700

    P10_Percobaan 1_Pertanyaan 7_1C_25

--- P10/Praktikum1/Queue25.java
+++ P10/Praktikum1/Queue25.java
@@ -59,6 +59,7 @@ public class Queue25 {
     public void enqueue(int dt){
         if (isFull()) {
             System.out.println("Queue sudah penuh");
+            System.exit(0);
         }else{
             if (isEmpty()) {
                 front = rear =0;
@@ -77,6 +78,7 @@ public class Queue25 {
         int dt = 0;
         if (isEmpty()) {
             System.out.println("Queue masih kosong");
+            System.exit(0);
         }else {
             dt = data[front];
             size--;

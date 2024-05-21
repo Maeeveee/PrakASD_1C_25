5971ce733585b79396ea5f909360905014d97cfd
Maeeveee <142636445+Maeeveee@users.noreply.github.com>
2024-05-21 18:58:32 +0700

    P12_Percobaan 3_1C_25

--- P12/doubleLinkedList25.java
+++ P12/doubleLinkedList25.java
@@ -133,4 +133,30 @@ public class doubleLinkedList25 {
             size --;
         }
     }
+    public int getFirst()throws Exception{
+        if (isEmpty()) {
+            throw new Exception ("Linked list masih kosong");
+        }
+        return head.data;
+    }
+    public int getLast()throws Exception{
+        if (isEmpty()) {
+            throw new Exception ("Linked list masih kosong");
+        }
+        node25 tmp = head;
+        while (tmp.next != null) {
+            tmp = tmp.next;
+        }
+        return tmp.data;
+    }
+    public int get(int index)throws Exception{
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai indeks di luar batas");
+        }
+        node25 tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        return tmp.data;
+    }
 }
--- P12/doubleLinkedListMain25.java
+++ P12/doubleLinkedListMain25.java
@@ -36,6 +36,24 @@ public class doubleLinkedListMain25 {
         System.out.println("==================================");
         dll.remove(1);
         dll.print();
-        System.out.println("size : "+dll.size());        
+        System.out.println("size : "+dll.size());     
+        dll.clear();
+         
+        dll.print();
+        System.out.println("size : "+dll.size());
+        System.out.println("==================================");
+        dll.addFirst(3);
+        dll.addLast(4);
+        dll.addFirst(7);
+        dll.print();
+        System.out.println("size : "+dll.size());
+        System.out.println("==================================");
+        dll.add(40, 1);
+        dll.print();
+        System.out.println("size : "+dll.size());
+        System.out.println("==================================");
+        System.out.println("Data awal linked list adalah : "+dll.getFirst());
+        System.out.println("Data akhir linked list adalah : "+dll.getLast());
+        System.out.println("data indeks ke-1 adalah : "+dll.get(1));  
     }
 }

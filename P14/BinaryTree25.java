76189d0c70aa5c37742541f4a6c43dcd5dd3d776
Maeeveee <142636445+Maeeveee@users.noreply.github.com>
2024-05-30 08:26:35 +0700

    P14_percobaan 1_1C_25

---
+++ P14/BinaryTree25.java
@@ -0,0 +1,153 @@
+package P14;
+
+public class BinaryTree25 {
+    Node25 root;
+    int size;
+
+    public BinaryTree25() {
+        root = null;
+
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public void add(int data) {
+        if (isEmpty()) {
+            root = new Node25(data);
+        }else{
+            Node25 current = root;
+            while(true){
+                if (data<current.data) {
+                    if (current.left == null) {
+                        current.left = new Node25(data);
+                        break;
+                    }else{
+                        current = current.left;
+                    }
+                }else if (data>current.data) {
+                    if (current.right == null) {
+                        current.right = new Node25(data);
+                    }else{
+                        current = current.right;
+                    }
+                }else{
+                    break;
+                }
+            }
+        }
+    }
+    public boolean find(int data) {
+        Node25 current = root;
+        while (current != null) {
+            if (current.data == data) {
+                return true;
+            }else if (data<current.data) {
+                current = current.left;
+            }else{
+                current = current.right;
+            }
+        }
+        return false;
+    }
+    public void traversePreOrder(Node25 node) {
+        if (node != null) {
+            System.out.print(" "+node.data);
+            traversePreOrder(node.left);
+            traversePreOrder(node.right);
+        }
+    }
+    public void traversePostOrder(Node25 node) {
+        if (node != null) {
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            System.out.print(" "+node.data);
+        }
+    }
+    public void traverseInOrder(Node25 node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            System.out.print(" "+node.data);
+            traverseInOrder(node.right);
+        }
+    }
+    public Node25 getSuccessor(Node25 del){
+        Node25 successor = del.right;
+        Node25 successorParent = del;
+        while (successor.left != null) {
+            successorParent = successor;
+            successor = successor.left;
+        }
+        if (successor != del.right) {
+            successorParent.left = successor.right;
+            successor.right = del.right;
+        }
+        return successor;
+    }
+
+    public void delete(int data) {
+        if (isEmpty()) {
+            System.out.println("Tree kosong");
+            return;
+        }
+        Node25 parent = root;
+        Node25 current = root;
+        boolean isLeftChild = false;
+        while (current != null&&current.data!=data) {
+            parent = current;
+            if (data<current.data) {
+                current = current.left;
+                isLeftChild = true;
+            }else if (data>current.data) {
+                current = current.right;
+                isLeftChild = false;
+            }
+        }
+        if (current == null) {
+            System.out.println("tidak dapat menemukan data");
+            return;
+        }else{
+            if (current.left == null && current.right == null) {
+                if (current==root) {
+                    root = null;
+                }else{
+                    if (isLeftChild) {
+                        parent.left = null;
+                    }else{
+                        parent.right = null;
+                    }
+                }
+            }else if (current.left==null) {
+                if (current==root) {
+                    root = current.right;
+                }else if (isLeftChild) {
+                    parent.left = current.right;
+                }else{
+                    parent.right = current.right;
+                }
+            }else if (current.right==null) {
+                if (isLeftChild) {
+                    root = current.left;
+                }else if (isLeftChild) {
+                    parent.left = current.left;  
+                }else{
+                    parent.right = current.left;
+                }
+            }else{
+                Node25 successor = getSuccessor(current);
+                if (current==root) {
+                    root = successor;
+                }else{
+                    if (isLeftChild) {
+                        parent.left = successor;
+                    }else{
+                        parent.right = successor;
+                    }
+                    successor.left = current.left;
+                }
+            }
+        }
+    }
+
+}
---
+++ P14/Node25.java
@@ -0,0 +1,12 @@
+package P14;
+
+public class Node25 {
+    int data;
+    Node25 left, right;
+
+    public Node25(int data){
+        this.left = null;
+        this.data = data;
+        this.right = null;
+    }
+}
